1ec467ed76c806ccca9019e82ec3a04c45a51de3
Biomorinnio <dkrysanov04@gmail.com>
2024-02-04 23:36:33 +0300

    sdgsdgf

--- js/complex-item.js
+++ js/complex-item.js
@@ -10,6 +10,33 @@ var swiperFeatures = new Swiper(".swiper-features", {
  
   });
   
+  function openedPopup() {
+    scrollY = window.scrollY;
+    document.body.style.top = `-${window.scrollY}px`;
+    document.body.style.position = "fixed";
+    document.body.style.width = "100%";
+  }
+  
+  function closedPopup() {
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.width = "";
+    window.scrollTo(0, scrollY);
+  }
+  
+  
+  document.querySelector('.developer__item .developer__item-btn').addEventListener('click', ()=>{
+    document.getElementById('openModal').classList.add('active');
+    shadow.classList.add('active')
+    openedPopup()
+  })
+  
+  shadow.addEventListener('click', ()=>{
+    document.getElementById('openModal').classList.remove('active');
+    shadow.classList.remove('active')
+    closedPopup()
+  })
+  
 
   const tableScrollBtn = document.querySelector('.payment__btn');
   const tableWrapper = document.querySelector('.scrollable-table')
