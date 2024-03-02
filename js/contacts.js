0bb223655669d125949e591db1d234b9f39737d8
Biomorinnio <dkrysanov04@gmail.com>
2024-03-02 16:11:27 +0300

    cont

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,7 @@
 const swiperContacts = new Swiper('.swiper-contacts', {
 
+  spaceBetween: 20,
+
     pagination: {
       el: '.swiper-pagination.contacts',
     },
@@ -8,6 +10,11 @@ const swiperContacts = new Swiper('.swiper-contacts', {
       nextEl: '.swiper-button-next.contacts',
       prevEl: '.swiper-button-prev.contacts',
     },
+    breakpoints: {
+    1181: {
+      spaceBetween: 0,
+  }
+  }
   
   });
 
