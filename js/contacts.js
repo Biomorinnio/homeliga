118fab3c95587e9c15048e605419ad2048ccecd8
Biomorinnio <dkrysanov04@gmail.com>
2024-03-11 16:37:40 +0300

    we

--- js/contacts.js
+++ js/contacts.js
@@ -1,6 +1,7 @@
 const swiperContacts = new Swiper('.swiper-contacts', {
 
   spaceBetween: 20,
+  loop: true,
 
     pagination: {
       el: '.swiper-pagination.contacts',
@@ -19,9 +20,16 @@ const swiperContacts = new Swiper('.swiper-contacts', {
   });
 
 const contactsTabs = document.querySelectorAll('.contacts__btn')
+const contactsTitle = document.querySelector('.contacts__descr-title')
 
 for(let i of contactsTabs) i.addEventListener('click', ()=>{
     for(let j of contactsTabs) j.classList.remove('active')
     i.classList.add('active')
+
+    let arrText = contactsTitle.textContent.split(' ')
+    arrText.splice(3, 1)
+    arrText.push(i.textContent.split(' ')[i.textContent.split(' ').length-1])
+    contactsTitle.textContent =  arrText.join(' ')
+  
 })
 
